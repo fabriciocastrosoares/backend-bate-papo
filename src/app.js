201e14a925208cbceb59ff26236795425e464849
fabriciocastrosoares <fabriciosoares27@gmail.com>
2025-06-23 17:01:43 -0300

    post atualizar status adicionado

--- src/app.js
+++ src/app.js
@@ -12,20 +12,30 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-let db;
+
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message));
+try {
+    await mongoClient.connect();
+    console.log("MongoDB conectado");
+} catch {
+    console.log(err.message);
+}
+const db = mongoClient.db();
 
 app.post("/participants", async (req, res) => {
     const { name } = req.body;
 
-    if (!name || typeof name !== "string") {
-        return res.status(422).send("Seu nome é obrigatório!");
-    }
+    const userSchema = joi.object({
+        name: joi.string().required()
+    });
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    };
 
     const newUser = { name, lastStatus: Date.now() };
+
     const newMessageStatus = {
         from: name,
         to: "Todos",
@@ -59,10 +69,17 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
 
     const { to, text, type } = req.body;
-    const {from} = req.headers;
+    const { from } = req.headers;
 
-    if (!to || typeof to !== "string" || !text || typeof text !== "string" || (type !== "message") && (type !== "private_message")) {
-        return res.status(422).send("É obrigatório escolher o destinátário e não pode conter uma menssagem vazia!");
+    const messageShema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid("message", "private_message").required()
+    });
+    const validation = messageShema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
     }
 
     const newMessage = {
@@ -72,19 +89,55 @@ app.post("/messages", async (req, res) => {
         type,
         time: dayjs().format("HH:mm:ss")
     }
-    try{
+    try {
         const participantExists = await db.collection("participants").findOne({ name: from });
         if (!participantExists) return res.sendStatus(422);
         await db.collection("messages").insertOne(newMessage);
         res.sendStatus(201);
-    }catch (err){
+    } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
 app.get("/messages", async (req, res) => {
+    const limit = Number(req.query.limit);
+    const user = req.header("User");
 
+    if (req.query.limit && (isNaN(limit) || limit < 1)) {
+        return res.sendStatus(422);
+    }
+    try {
+        const messages = await db.collection("messages").find({
+            $or: [
+                { type: "message" },
+                { to: "Todos" },
+                { to: user },
+                { from: user }
+            ]
+        })
+            .limit(limit || 0)
+            .toArray();
+        res.send(messages);
+    } catch (err) {
+        res.sendStatus(500);
+    }
 });
+
+app.post("/status", async (req, res) => {
+    const { user } = req.headers;
+    const updateUser = { name: user, lastStatus: Date.now() }
+    console.log(user);
+    if (!user) return res.sendStatus(404);
+    try {
+        const existingUser = await db.collection("participants").findOne({ name: user });
+        if (!existingUser) return res.sendStatus(404);
+        await db.collection("participants").updateOne({ name: user }, { $set: updateUser });
+        res.sendStatus(200);
+    } catch {
+        res.sendStatus(500);
+    }
+
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
